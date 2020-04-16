5bcdc412cf3e346d28442840f97d4b68c2814ace
adilu khalid <adiluabdul@gmail.com>
2020-04-17 00:46:21 +0300

    Added handlebars and bootstrap starter

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
-
+const exphbs  = require('express-handlebars')
 const app = express()
-
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
 const PORT = process.env.PORT 
 
+app.get("/",(req, res, next)=>{
+	res.render('index')
+})
 app.listen(PORT,()=>{
 	console.log(`Server started at ${PORT}`)
 })
